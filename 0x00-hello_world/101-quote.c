e02edcf9424e30c747ef0c3dd7439055aa4f73db
sirln <siro@sirlawren.com>
2023-03-16 22:29:19 +0000

    C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, text, 59);
 	return (1);
