4b1239e5fb17c4cad16c0cfd2af8a95f78bd9fb5
sirln <siro@sirlawren.com>
2023-03-16 22:11:02 +0000

    C - Hello, World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - program entry point
+ *
+ * Return: return 1
+ */
+
+int main(void)
+{
+	write(STDERR_FILENO,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
