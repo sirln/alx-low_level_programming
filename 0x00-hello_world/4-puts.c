a0832ac5c063e03db29b7dcd2cfe883d73c7d4a3
sirln <siro@sirlawren.com>
2023-03-16 18:43:15 +0000

    C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * program that prints:
+ * 'Programming is like building a multilingual puzzle'
+ */
+
+int main(void)
+{
+	puts('Programming is like building a multilingual puzzle');
+	return (0);
+}
