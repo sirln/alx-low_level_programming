3c1f4a14e6b03781569064336c04aa3fd8972d51
sirln <siro@sirlawren.com>
2023-03-17 13:57:35 +0000

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main -  program start point
+ *
+ *Return: print number stored in variable n
+ *
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+		putchar('\n');
+	}
+	return (0);
+}
