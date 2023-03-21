d1db34717771e723c5543ca3dd71452c12711e0c
sirln <siro@sirlawren.com>
2023-03-21 18:53:29 +0000

    C - Functions, nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,7 @@
 #include  "main.h"
 /**
- *_abs -  computes the absolute value of an integer.
+ *jack_bauer -  prints every minute of the day of Jack Bauer,
+ *              starting from 00:00 to 23:59.
  *
  */
 void jack_bauer(void)
