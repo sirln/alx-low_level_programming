93ecb9904ab1c2e73f7031212d1ef35d698115e6
sirln <siro@sirlawren.com>
2023-03-21 15:39:30 +0000

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include  "main.h"
+/**
+ *main -  program start point
+ *
+ *Return: always return 0
+ *
+ */
+void print_alphabet(void)
+{
+	char s;
+
+	for (s = 'a'; s <= 'z'; s++)
+	{
+		_putchar(s);
+	}
+
+	_putchar('\n');
+}
