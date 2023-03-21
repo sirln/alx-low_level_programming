ba33e182ed30f6982037556cb5919399faef685a
sirln <siro@sirlawren.com>
2023-03-21 16:35:01 +0000

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include  "main.h"
+/**
+ *print_alphabet_x10 -  function to print the alphabets 10x
+ *
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -20,4 +20,10 @@ void print_alphabet_x10(void);
   *
   */
 
+int _islower(int c);
+/**
+  * checks for lowercase character.
+  *
+  */
+
 #endif
