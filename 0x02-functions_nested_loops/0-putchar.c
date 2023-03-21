a6eda581b7ee77f7b1d7420b4d87fb4d2613e866
sirln <siro@sirlawren.com>
2023-03-21 14:16:57 +0000

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include  "main.h"
+/**
+ *main -  program start point
+ *
+ *Return: print number stored in variable n
+ *
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
