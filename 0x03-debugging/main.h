9985a0930920ab653e5f44df896651c4fd0035f7
sirln <siro@sirlawren.com>
2023-03-23 16:02:49 +0000

    C - Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,11 +4,10 @@
 #include <stdio.h>
 
 int positive_or_negative(int i);
-
-int largest_number(int a, int b, int c);
+/*int largest_number(int a, int b, int c);
 
 int convert_day(int month, int day);
 
-void print_remaining_days(int month, int day, int year);
+void print_remaining_days(int month, int day, int year);*/
 
 #endif /* MAIN_H */
