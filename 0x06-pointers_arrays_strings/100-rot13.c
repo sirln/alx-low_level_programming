75233c2cca82a86b3a8fe34b800df570f0a83447
sirln <siro@sirlawren.com>
2023-03-30 15:41:05 +0000

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+/**
+ *rot13 - encodes a string into 1337.
+ *@enc: parameter
+ *Return: encoded string
+ */
+char *rot13(char *enc)
+{
+	int l = 0;
+
+	while (enc[l] != '\0')
+	{
+		if ((enc[l] >= 'a' && enc[l] <= 'm') || (enc[l] >= 'A' && enc[l] <= 'M'))
+		{
+			enc[l] += 13;
+		}
+		else if ((enc[l] >= 'n' && enc[l] <= 'z') || (enc[l] >= 'N' && enc[l] <= 'Z'))
+		{
+			enc[l] -= 13;
+		}
+		l++;
+
+	}
+	return (enc);
+}
