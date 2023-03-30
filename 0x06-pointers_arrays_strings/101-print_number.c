4c07bef15d2128db4d3b0a4d982a539ee4afdadc
sirln <siro@sirlawren.com>
2023-03-30 16:25:11 +0000

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,15 +6,17 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	unsigned int num = n;
+
+	if (num < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -num;
 	}
-	if (n / 10 > 0)
+	if (num / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(num / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar(num % 10 + '0');
 
 }
