61e492236075c88990c8e78b15d362ac12d64883
sirln <siro@sirlawren.com>
2023-03-30 16:18:58 +0000

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -237,5 +237,66 @@ char *_strncpy(char *dest, char *src, int n);
   *
   */
 
+int _strcmp(char *s1, char *s2);
+/**
+  * function that compares two strings.
+  *
+  */
+
+void reverse_array(int *a, int n);
+/**
+  * function that reverses the content of an array of integers.
+  *
+  */
+
+char *string_toupper(char *);
+/**
+  * function that changes all lowercase letter of a string to uppercase
+  *
+  */
+
+char *cap_string(char *);
+/**
+  * function that capitalizes all words of a string
+  * separators of words: `space`, `tabulation`, `new line`, `,`,`;`,
+  *			`.`, `!`, `?`, `"`, `(`, `)`, `{`, and `}`
+  */
+
+char *leet(char *);
+/**
+  * function that encodes a string into 1337.
+  * Letters a and A should be replaced by 4
+  * Letters e and E should be replaced by 3
+  * Letters o and O should be replaced by 0
+  * Letters t and T should be replaced by 7
+  * Letters l and L should be replaced by 1
+  * Prototype: char *leet(char *);
+  * You can only use one if in your code
+  * You can only use two loops in your code
+  * You are not allowed to use switch
+  * You are not allowed to use any ternary operation
+  */
+
+char *rot13(char *);
+/**
+  *function that encodes a string using rot13.
+ * Prototype: char *rot13(char *);
+ * You can only use if statement once in your code
+ * You can only use two loops in your code
+ * You are not allowed to use switch
+ * You are not allowed to use any ternary operation
+ */
+
+void print_number(int n);
+/**
+  * function that prints an integer.
+  * Prototype: void print_number(int n);
+  * You can only use _putchar function to print
+  * You are not allowed to use long
+  * You are not allowed to use arrays or pointers
+  * You are not allowed to hard-code special values
+  */
+
+
 
 #endif
