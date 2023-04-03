5318469b25c56e20046a004a59336a6bb11a4776
sirln <siro@sirlawren.com>
2023-04-03 18:30:36 +0000

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,20 +9,14 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	if (*needle == 0)
-	{
-		return (haystack);
-	}
-
 	while (*haystack)
 	{
-		int n = 0;
-
-		if (*haystack == needle[n])
+		char *n = needle;
+		if (*haystack == *n)
 		{
-			while (*haystack == needle[n])
+			while (*haystack == *n)
 			{
-				if (*haystack + 1 == '\0')
+				if (*haystack == *n)
 				{
 					return (haystack);
 				}
