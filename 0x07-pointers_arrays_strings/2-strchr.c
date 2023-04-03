e3dfb0580d16b1f8052d8b49c37a5e19242e40fe
sirln <siro@sirlawren.com>
2023-04-03 17:59:12 +0000

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,15 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s)
+	int x = 0;
+
+	while (s[x] >= '\0')
 	{
-		if (*s == c)
+		if (s[x] == c)
 		{
-			return (s);
+			return (s + x);
 		}
-		s++;
+		x++;
 	}
 
 	return ('\0');
