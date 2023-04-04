0c183836fa5413cfa67d1d6a3a4aa1228349a13c
sirln <siro@sirlawren.com>
2023-04-04 12:52:40 +0000

    C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
