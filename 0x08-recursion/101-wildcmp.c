993667b8739d3e9c1153f37e4c138fe0c76ce4c4
sirln <siro@sirlawren.com>
2023-04-06 01:02:37 +0000

    C - Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,23 +14,19 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == '\0')
 		return (*s1 == '\0');
 
-	else if (*s2 == '*')
+	if (*s2 == '*')
 	{
-		while (*s1 != '\0')
-		{
-			if (wildcmp(s1, s2 + 1))
-				return (1);
-			s1++;
-		}
-		return (wildcmp(s1, s2 + 1));
-	}
-
-	else
-	{
-		if (*s1 == *s2)
-			return (wildcmp(s1 + 1, s2 + 1));
+		if (*s1 == '\0')
+			return (wildcmp(s1, s2 + 1));
 		else
-			return (0);
+			return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
 
+	if (*s1 == '\0')
+		return (0);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	return (0);
 }
