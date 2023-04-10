4c2c526d5f8f282b51195175ddfb4361d6c2b09f
sirln <siro@sirlawren.com>
2023-04-10 23:57:33 +0000

    C - argc, argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 /**
   *main -  prints the minimum number of coins to make
   *	change for an amount of money
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 /**
   *main - multiplies two numbers.
   *@argc : number of parameters/arguments
