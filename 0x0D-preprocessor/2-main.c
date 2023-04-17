0509d6c1ffe1dd3b77a5bc38000eab2136616ea7
sirln <siro@sirlawren.com>
2023-04-17 17:14:49 +0000

    C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - start of program
+  *
+  *Return: 0;
+  *
+  */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef ABS(x)
+
+#define ABS(x) ((x) < 0 ? -1 * (x) : (x))
+
+#endif
