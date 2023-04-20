c7b853a208fcc8eaed6f1087c0cf9bd80c7a2dc8
sirln <siro@sirlawren.com>
2023-04-20 14:49:45 +0000

    C - Variadic functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,9 +14,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int l;
 
-	if (n == 0 || !separator)
-		return;
-
+	/**
+	*if (n == 0 || !separator)
+	*	return;
+	*
+	*/
 	va_start(args, n);
 
 	for (l = 0; l < n; l++)
