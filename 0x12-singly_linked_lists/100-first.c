624116bcba8da67814a6e4e969ef4f61f819b425
sirln <siro@sirlawren.com>
2023-04-27 23:31:58 +0000

    Task 5 for C - Singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 
-void print_first()
+/**
+  * print_first - function to run
+  */
+
+void print_first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -26,5 +26,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-void print_first()__attribute__ ((constructor));
+void print_first(void)__attribute__ ((constructor));
 #endif
