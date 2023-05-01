6e97098d7961dda5ca43863a45a399ca4d0f325a
sirln <siro@sirlawren.com>
2023-05-01 22:29:55 +0000

    Task 9 C - More singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return	(NULL);
 
 	new_list->n = n;
-
+	new_list->next = NULL;
 	if (idx == 0)
 	{
 		new_list->next = *head;
