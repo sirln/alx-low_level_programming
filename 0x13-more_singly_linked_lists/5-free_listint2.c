f1da7b64a3b90bf9725c412ecff44166604650ea
sirln <siro@sirlawren.com>
2023-05-01 17:15:04 +0000

    Task 5 C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+  *free_listint2 - frees a list
+  *
+  *@head: pointer to pointer to head node in list
+  *
+  */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (!(*head))
+		return;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
