e594e8f48db9d4f99685900946ba55aefec69722
sirln <siro@sirlawren.com>
2023-05-03 02:39:54 +0000

    Task 13 C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+  *free_listint_safe - frees a linked list
+  *
+  *@h: pointer to pointer to head node in list
+  *
+  *Return: number of nodes
+  */
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t num_nodes = 0;
+	listint_t *temp;
+
+	if (!h || !(*h))
+		return(0);
+
+	while (*h)
+	{
+		num_nodes++;
+		temp = (*h)->next;
+
+		if (temp >= *h)
+		{
+			free(*h);
+			*h = NULL;
+			exit(98);
+		}
+		free(*h);
+		*h = temp;
+	}
+	return (num_nodes);
+}
+
