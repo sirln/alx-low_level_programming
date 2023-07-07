e850177992d45229464a509bc6a6e7331fb5513e
sirln <siro@sirlawren.com>
2023-07-07 17:23:59 +0000

    C - Hash tables. Task 5. >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+  *hash_table_print - print a hash table
+  *
+  *@ht: hash table
+  *
+  */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int index = 0, l;
+
+	if (!ht)
+		return;
+
+	printf("{");
+	while (index < ht->size)
+	{
+		node = ht->array[index];
+		while (node)
+		{
+			if (l == 1)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			l = 1;
+			node = node->next;
+		}
+		index++;
+	}
+	printf("}\n");
+}
