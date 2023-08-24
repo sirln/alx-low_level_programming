b24a6a63886ca502f370d1367b3ecc04d7080a28
sirln <siro@sirlawren.com>
2023-08-24 03:35:29 +0300

    C - Makefiles, Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''
+Island Perimeter Module
+'''
+
+def island_perimeter(grid):
+    '''
+    Get the perimeter of the island
+
+    Arguments
+    ---------
+    grid: list
+        a list of list of integers
+
+    Return
+    ------
+    int:
+        perimeter of the island
+    '''
+    if not grid:
+        return 0
+
+    rows = len(grid)
+    columns = len(grid[0])
+    perimeter = 0
+
+    for r in range(rows):
+        for c in range(columns):
+            if grid[r][c] == 1:
+                if r == 0 or grid[r - 1][c] == 0:
+                    perimeter += 1
+                if r == rows -1 or grid[r + 1][c] == 0:
+                    perimeter += 1
+                if c == 0 or grid[r][c - 1] == 0:
+                    perimeter += 1
+                if c == columns or grid[r][c + 1] == 0:
+                    perimeter += 1
+    return perimeter
