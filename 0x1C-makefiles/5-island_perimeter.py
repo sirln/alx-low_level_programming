b68ecd5748af63273ba210ab32b09bf217be8968
sirln <siro@sirlawren.com>
2023-08-24 03:49:56 +0300

    C - Makefiles, Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -19,21 +19,21 @@ def island_perimeter(grid):
         perimeter of the island
     '''
     rows = len(grid)
-    columns = len(grid[0])
+    cols = len(grid[0])
     perimeter = 0
 
     if not grid:
         return perimeter
 
     for r in range(rows):
-        for c in range(columns):
+        for c in range(cols):
             if grid[r][c] == 1:
                 if r == 0 or (r > 0 and grid[r - 1][c] == 0):
                     perimeter += 1
-                if r == rows - 1 or (r < rows -1 and  grid[r + 1][c] == 0):
+                if r == rows - 1 or (r < rows - 1 and grid[r + 1][c] == 0):
                     perimeter += 1
-                if c == 0 or (c > 0 and  grid[r][c - 1] == 0):
+                if c == 0 or (c > 0 and grid[r][c - 1] == 0):
                     perimeter += 1
-                if c == columns -1 or (c < columns -1 and grid[r][c + 1] == 0):
+                if c == cols - 1 or (c < cols - 1 and grid[r][c + 1] == 0):
                     perimeter += 1
     return perimeter
